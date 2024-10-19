cb00dec25f45a5f85a539c4408498a73ba489060
Kaan Kara <kaankara1987@gmail.com>
2024-10-19 19:19:37 +0300

    initial

---
+++ src/main/java/org/xhite/proactive/ProactiveApplication.java
@@ -0,0 +1,13 @@
+package org.xhite.proactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xhite/proactive/ProactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xhite.proactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
