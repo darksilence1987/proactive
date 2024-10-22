05b7a913ed91f91ae4519fb741a003d5fd286dff
Kaan Kara <kaankara1987@gmail.com>
2024-10-22 15:09:36 +0300

    test user initialized and login page is added

--- src/main/java/org/xhite/proactive/config/DataInitializer.java
+++ src/main/java/org/xhite/proactive/config/DataInitializer.java
@@ -3,16 +3,26 @@ package org.xhite.proactive.config;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.xhite.proactive.user.AppUser;
+import org.xhite.proactive.user.UserRepository;
+import org.xhite.proactive.user.UserStatus;
 import org.xhite.proactive.user.role.Role;
 import org.xhite.proactive.user.role.RoleName;
 import org.xhite.proactive.user.role.RoleRepository;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 public class DataInitializer {
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostConstruct
     public void initializeRoles(){
@@ -25,5 +35,23 @@ public class DataInitializer {
                     roleRepository.save(role);
 
                 });
+    }
+
+    @PostConstruct
+    public void initalizeTestUser(){
+        AppUser user = new AppUser();
+        user.setUsername("xhite");
+        user.setPassword(passwordEncoder.encode("1234"));
+        user.setStatus(UserStatus.ACTIVE);
+        Role role = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.getRoles().add(role);
+        role = roleRepository.findByName(RoleName.ROLE_PROJECT_MANAGER)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.getRoles().add(role);
+        role = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.getRoles().add(role);
+        userRepository.save(user);
     }
 }
--- src/main/java/org/xhite/proactive/user/role/RoleRepository.java
+++ src/main/java/org/xhite/proactive/user/role/RoleRepository.java
@@ -3,7 +3,11 @@ package org.xhite.proactive.user.role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     boolean existsByName(RoleName roleName);
+
+    Optional<Role> findByName(RoleName roleName);
 }
